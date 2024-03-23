87ace89860d7fdd931bf81b14d54f7f7367069b8
GerardWassink <GerardWassink@users.noreply.github.com>
2024-03-23 17:10:21 +0100

    Keypad code

--- GAW-RPN-Calculator.ino
+++ GAW-RPN-Calculator.ino
@@ -52,11 +52,11 @@
 #define ROWS 5                              // five rows for keyboard
 #define COLS 8                              // eight columns for keyboard
 
-/* ------------------------------------------------------------------------- *
- *                                                         Include libraries
- * ------------------------------------------------------------------------- */
+
 #include <Wire.h>                       // I2C comms library
 #include <LiquidCrystal_I2C.h>          // LCD library
+
+#include <Keypad.h>                     // Keypad library
 
 #include <String.h>                     // String library
 
@@ -82,7 +82,10 @@ const unsigned int statRAD=1;
 const unsigned int statGRD=2;
 unsigned int gonioStatus = statDEG;     // default to degrees
 
-bool stateShift=0;                      // shift default to 0 (off)
+const unsigned int noShift=0;
+const unsigned int shiftF=1;
+const unsigned int shiftG=2;
+unsigned int stateShift=noShift;        // shift default to 0 (off)
 
 double E = 2.718281828459045;           // constant for the natural number
 
@@ -108,10 +111,13 @@ double Reg[30];
  *  Key values indicate row and column number
  * ------------------------------------------------------------------------- */
   char keys[ROWS][COLS] = {
-    {0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18},
-    {0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28},
-    {0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38},
-    {0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48}
+  //   7     8     9    10    11    12    13    14     <== pins
+  //   1     2     3     4     5     6     7     8     <== columns
+    {0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}, // row 1, pin 2
+    {0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28}, // row 2, pin 3
+    {0x31, 0x32, 0x33, 0x34, 0x35, 0xFF, 0x37, 0x38}, // row 3, pin 4
+    {0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48}, // row 4, pin 5
+    {0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58}, // row 5, pin 6
   };
   byte rowPins[ROWS] = {2,3,4,5,6};               // row pins of the keypad
   byte colPins[COLS] = {7,8,9,10,11,12,13,17};    // column pins of the keypad
@@ -120,13 +126,6 @@ double Reg[30];
  *       Create object for Keypad
  * ------------------------------------------------------------------------- */
   Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-/* ------------------------------------------------------------------------- *
- *                                                                    loop()
- * ------------------------------------------------------------------------- */
-void loop() {
-  delay(5000);
-}
 
 
 /* ------------------------------------------------------------------------- *
@@ -171,16 +170,19 @@ void setup() {
   push(2);
   showStack();
 
+delay(1000);
+
   STO(0);
   showStack();
 
+delay(1000);
+
   push(3);
   push(4);
+  showStack();
 
   RCL(0);
   showStack();
-
-delay(5000);
 
 /*
   String test1 = "-123.456789";
@@ -195,6 +197,137 @@ delay(5000);
 
 }
 
+/* ------------------------------------------------------------------------- *
+ *                                                                    loop()
+ * ------------------------------------------------------------------------- */
+void loop() {
+
+  /* 
+   * Read key from keypad
+   */
+  String showchar="";
+  char key = keypad.getKey();               // get key press
+  
+//  if (key != NO_KEY){                       // did we receive one?
+  if (key) {  
+    showchar = String(key, HEX);
+    debugln(showchar + " - key pressed");
+    switch (stateShift) {
+      case noShift: {
+        switch (key) {
+
+          case 0x11: { SQRT();        break; }
+          case 0x12: { EtoX();        break; }
+          case 0x13: { TENtoX();      break; }
+          case 0x14: { POW();         break; }
+          case 0x15: { OneOverX();    break; }
+          case 0x16: { CHS();         break; }
+          case 0x17: { /*   7  */     break; }
+          case 0x18: { /*   8  */     break; }
+
+          case 0x21: { /* SST  */     break; }
+          case 0x22: { /* GTO  */     break; }
+          case 0x23: { SIN();         break; }
+          case 0x24: { COS();         break; }
+          case 0x25: { TAN();         break; }
+          case 0x26: { /* EEX  */     break; }
+          case 0x27: { /*   4  */     break; }
+          case 0x28: { /*   5  */     break; }
+
+          case 0x31: { /* R/S   */    break; }
+          case 0x32: { /* GSB   */    break; }
+          case 0x33: { rollDown();    break; }
+          case 0x34: { swapXY();      break; }
+          case 0x35: { /* BSP   */    break; }
+          case 0x36: { /* ENTER */    break; }
+          case 0x37: { /*   1   */    break; }
+          case 0x38: { /*   2   */    break; }
+
+          case 0x41: { /* ON   */     break; }
+          case 0x42: { /* f    */     break; }
+          case 0x43: { /* g    */     break; }
+          case 0x44: { /* STO  */     break; }
+          case 0x45: { /* RCL  */     break; }
+          case 0x46: { /* ENTER*/     break; }
+          case 0x47: { /*   0  */     break; }
+          case 0x48: { /*   .  */     break; }
+
+          case 0x51: { /*   9  */     break; }
+          case 0x52: { DIVIDE();      break; }
+          case 0x53: { /*   6  */     break; }
+          case 0x54: { MULTIPLY();    break; }
+          case 0x55: { /*   3  */     break; }
+          case 0x56: { SUBTRACT();    break; }
+          case 0x57: { /* SIGMA*/     break; }
+          case 0x58: { ADD();         break; }
+
+          default: { break; }
+        }
+      }
+
+      case shiftF: {
+        switch (key) {
+
+          case 0x11: { /*   A  */     break; }
+          case 0x12: { /*   B  */     break; }
+          case 0x13: { /*   C  */     break; }
+          case 0x14: { /*   D  */     break; }
+          case 0x15: { /*   E  */     break; }
+          case 0x16: { /*MATRIX*/     break; }
+          case 0x17: { /* FIX  */     break; }
+          case 0x18: { /* SCI  */     break; }
+
+          case 0x21: { /* LBL  */     break; }
+          case 0x22: { /* HYP  */     break; }
+          case 0x23: { /* DIM  */     break; }
+          case 0x24: { /* (i)  */     break; }
+          case 0x25: { /* I    */     break; }
+          case 0x26: { /*RESULT*/     break; }
+          case 0x27: { /* X><  */     break; }
+          case 0x28: { /* DSE  */     break; }
+
+          case 0x31: { /* PSE   */    break; }
+          case 0x32: { /* CL E  */    break; }
+          case 0x33: { /* CL PGM*/    break; }
+          case 0x34: { /* CL REG*/    break; }
+          case 0x35: { /* CL PFX*/    break; }
+          case 0x36: { /* RAN#  */    break; }
+          case 0x37: { /* => R  */    break; }
+          case 0x38: { /* =>HMS */    break; }
+
+          case 0x41: { /* ON   */     break; }
+          case 0x42: { /* f    */     break; }
+          case 0x43: { /* g    */     break; }
+          case 0x44: { /* FRAC */     break; }
+          case 0x45: { /* USER */     break; }
+          case 0x46: { /* ENTER*/     break; }
+          case 0x47: { /* X!   */     break; }
+          case 0x48: { /* Y,r  */     break; }
+
+          case 0x51: { /* ENG  */     break; }
+          case 0x52: { /* SOLVE*/     break; }
+          case 0x53: { /* ISG  */     break; }
+          case 0x54: { /* f XY */     break; }
+          case 0x55: { /* =>RAD*/     break; }
+          case 0x56: { /* Re Im*/     break; }
+          case 0x57: { /* L.R. */     break; }
+          case 0x58: { /* P x,y*/     break; }
+
+          default: { break; }
+        }
+      }
+      case shiftG: {
+        switch (key) {
+          default: { break; }
+        }
+      }
+      default: { break; }
+    }
+    stateShift = noShift;
+    showStack();
+  }
+}
+
 
 /* ------------------------------------------------------------------------- *
  *                                                     Logarithmic functions
@@ -203,6 +336,7 @@ void LOG10()  { stack[X] = log10(stack[X]); }     // 10 log OF x
 void TENtoX() { stack[X] = pow(10, stack[X]); }   // 10 TO THE POWER OF x 
 void LOG()    { stack[X] = log(stack[X]); }       // natural log OF x
 void EtoX()   { stack[X] = pow(E, stack[X]); }    // e TO THE POWER OF x
+
 
 /* ------------------------------------------------------------------------- *
  *                                                      Algebraicic functions
@@ -221,7 +355,7 @@ void POW()  {                                     // Y to the power of X
   stack[X] = temp;
 }
 
-void PERCENT()  {                                     // Take X perecnt of Y
+void PERCENT()  {                                 // Take X perecnt of Y
   double temp = stack[Y] * stack[X] / 100;
   stack[X] = stack[T];
   rollDown();
@@ -255,6 +389,7 @@ void DIVIDE()  {                                  // Divide
   rollDown();
   stack[X] = temp;
 }
+
 
 /* ------------------------------------------------------------------------- *
  *                                                     Goniometric functions
@@ -366,13 +501,13 @@ void clearReg() {                                 // Clear registters
   }
 }
 
-double RCL(int reg) {
-  rollUp();
-  stack[X] = Reg[reg];
+void STO(int reg) {                               // Store X in register
+  Reg[reg] = stack[X];
 }
 
-double STO(int reg) {
-  Reg[reg] = stack[X];
+void RCL(int reg) {                               // Recall X from register
+  rollUp();
+  stack[X] = Reg[reg];
 }
 
 void FIX(int val) { precision = val; }
